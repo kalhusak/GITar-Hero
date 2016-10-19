3b096066c035861c064507625a14b9247ad14424
imosek8 <imosek8@o2.pl>
2016-10-19 13:45:38 +0200

    Console class name fix

--- src/components/Console/Console.js
+++ src/components/Console/Console.js
@@ -1,10 +1,17 @@
 import React, {Component, PropTypes} from 'react';
 import './Console.scss'
 
-class Canvas extends Component {
+class Console extends Component {
+
+  constructor(props){
+    super(props);
+
+    let x ='';
+  }
+
   render() {
     return (<textarea rows="4" cols="50" className='console' defaultValue='git reset--soft HEAD~3'/>)
   }
 };
 
-export default Canvas;
+export default Console;
