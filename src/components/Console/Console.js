f22ddb1cc539bc393c69dce6a6f59f2a457262c8
imosapatryk <imosa.patryk@gmail.com>
2016-10-19 12:55:12 +0100

    Console variables remove

--- src/components/Console/Console.js
+++ src/components/Console/Console.js
@@ -3,12 +3,6 @@ import './Console.scss'
 
 class Console extends Component {
 
-  constructor(props){
-    super(props);
-
-    let x ='';
-  }
-
   render() {
     return (<textarea rows="4" cols="50" className='console' defaultValue='git reset--soft HEAD~3'/>)
   }
