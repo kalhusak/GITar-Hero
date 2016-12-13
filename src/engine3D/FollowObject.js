4a5c8b259b363f16ab882f3750422bba782879e1
MLatkowska <magda@laszlo.pl>
2016-12-13 20:36:57 +0100

    Add particles emitter to background

--- src/engine3D/FollowObject.js
+++ src/engine3D/FollowObject.js
@@ -1,6 +1,7 @@
 import BABYLON from 'babylonjs'
 import MoveUtils from './utils/MoveUtils';
 import CameraTarget from './objects/CameraTarget';
+import Particles from './Particles';
 
 const config = {
   cameraTargetSpeed: 1.5
@@ -12,12 +13,16 @@ export default class FollowObject {
     this._moveCameraTargetToObject = ::this._moveCameraTargetToObject;
     this._isCameraNextToObject = ::this._isCameraNextToObject;
     this.addOffsetInPlace = ::this.addOffsetInPlace;
+    this._initParticles = ::this._initParticles;
 
     this.scene = scene;
     this.object3D = null;
     this.cameraTarget = new CameraTarget('HeadCameraTarget', scene);
     this.isMoving = false;
     this.offset = BABYLON.Vector3.Zero();
+
+    this.particles = null;
+    this._initParticles();
 
     this.scene.registerBeforeRender(() => {
       this._moveCameraTargetToObjectIfNeed();
@@ -45,5 +50,16 @@ export default class FollowObject {
   _isCameraNextToObject () {
     var targetPosition = this.object3D.getPosition().add(this.offset);
     return this.cameraTarget.getPosition().equals(targetPosition);
+  }
+
+  _initParticles () {
+    let targetParticles = new Particles('target', this.cameraTarget.mesh, this.scene);
+    targetParticles.setEmitParams(500, 2, 5, 0.01);
+    targetParticles.setLifeTimes(0.2, 0.6);
+    targetParticles.setDirections(new BABYLON.Vector3(3, 3, 3), new BABYLON.Vector3(-3, -3, -3));
+    targetParticles.setSizes(0.5,2);
+    targetParticles.stop();
+
+    this.particles = targetParticles;
   }
 }
--- src/engine3D/Scene.js
+++ src/engine3D/Scene.js
@@ -1,9 +1,53 @@
 import BABYLON from 'babylonjs';
+import Particles from './Particles';
+import flare from './textures/flare.png';
 
-export default class Scene extends BABYLON.Scene {
+const backgroundConfig = {
+  targetOffset: new BABYLON.Vector3(0, -40, 400),
+  texture: flare,
+  minEmitBox: new BABYLON.Vector3(-6, -6, 0),
+  maxEmitBox: new BABYLON.Vector3(6, 6, 0),
+  direction1: new BABYLON.Vector3(-50, -50, -100),
+  direction2: new BABYLON.Vector3(50, 50, -100),
+  minTime: 2,
+  maxTime: 6,
+
+}
+
+class Scene extends BABYLON.Scene {
   constructor (engine) {
     super(engine);
+
+    this.initBackgroundParticles = ::this.initBackgroundParticles;
+    this.updateBackgroundParticlesEmitter = ::this.updateBackgroundParticlesEmitter;
+    this.initFog = ::this.initFog;
+
     // TODO make light management object
     const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(3, 1, -3), this);
+    this.clearColor = new BABYLON.Color3(0.05, 0.05, 0.05);
+
+    this.initBackgroundParticles();
+    // this.initFog();
+  }
+
+  initFog () {
+    this.fogMode = BABYLON.Scene.FOGMODE_EXP;
+    this.fogDensity = 0.001;
+    this.fogColor = new BABYLON.Color3(0, 0, 0);
+  }
+
+  initBackgroundParticles () {
+    const { targetOffset, minEmitBox, maxEmitBox, direction1, direction2, minTime, maxTime } = backgroundConfig;
+    this.backgroundParticles = new Particles('background', targetOffset, this, flare);
+    this.backgroundParticles.setEmitBox(minEmitBox, maxEmitBox);
+    this.backgroundParticles.setDirections(direction1, direction2);
+    this.backgroundParticles.setLifeTimes(minTime, maxTime);
+  }
+
+  updateBackgroundParticlesEmitter (newPosition) {
+    let { targetOffset } = backgroundConfig;
+    this.backgroundParticles.emitter = newPosition.add(targetOffset);
   }
 }
+
+export default Scene;
