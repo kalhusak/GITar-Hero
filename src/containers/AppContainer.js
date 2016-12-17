a4e3135920f3a49d68acfcca2ada8a095e1e8079
Tymoteusz Bleja <tymoteusz.bleja@outlook.com>
2016-12-17 20:46:40 +0100

    Add time component

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -4,6 +4,7 @@ import Canvas from '../components/Canvas';
 import Console from '../components/Console';
 import TaskList from './TaskList';
 import Points from '../components/Points';
+import Time from '../components/Time';
 
 class AppContainer extends Component {
   static propTypes = {
@@ -13,28 +14,26 @@ class AppContainer extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      points: 0
+      points: 0,
+      time: 0
     };
     setTimeout(() => {
       this.setState({
-        points: 100
+        points: 0,
+        time: 10000
       });
-    }, 2000);
+    });
     setTimeout(() => {
       this.setState({
-        points: 300
-      });
-    }, 3000);
-    setTimeout(() => {
-      this.setState({
-        points: 500
-      });
-    }, 5000);
-    setTimeout(() => {
-      this.setState({
-        points: 200
+        points: 200,
+        time: 8000
       });
     }, 6000);
+    setTimeout(() => {
+      this.setState({
+        points: 400
+      });
+    }, 10000);
   }
 
   render () {
@@ -42,7 +41,8 @@ class AppContainer extends Component {
       <Provider store={this.props.store}>
         <div style={{ height: '100%' }}>
           <Canvas id='renderCanvas' store={this.props.store} />
-          <Points value={this.state.points} time={2000} />
+          <Points value={this.state.points} />
+          <Time time={this.state.time} />
           <Console />
           <TaskList />
         </div>
