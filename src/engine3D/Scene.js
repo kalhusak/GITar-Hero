6206d1e7812ea3d2ccd9238206f3c158a6d56c66
MLatkowska <magda@laszlo.pl>
2016-12-19 21:13:19 +0100

    Change particles emitter to box

--- src/engine3D/Scene.js
+++ src/engine3D/Scene.js
@@ -3,6 +3,7 @@ import Particles from './Particles';
 import flare from './textures/flare.png';
 
 const backgroundConfig = {
+  sceneColor: new BABYLON.Color3(0.05, 0.05, 0.05),
   targetOffset: new BABYLON.Vector3(0, -40, 400),
   texture: flare,
   minEmitBox: new BABYLON.Vector3(-6, -6, 0),
@@ -10,8 +11,7 @@ const backgroundConfig = {
   direction1: new BABYLON.Vector3(-50, -50, -100),
   direction2: new BABYLON.Vector3(50, 50, -100),
   minTime: 2,
-  maxTime: 6,
-
+  maxTime: 6
 }
 
 class Scene extends BABYLON.Scene {
@@ -22,9 +22,8 @@ class Scene extends BABYLON.Scene {
     this.updateBackgroundParticlesEmitter = ::this.updateBackgroundParticlesEmitter;
     this.initFog = ::this.initFog;
 
-    // TODO make light management object
-    const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(3, 1, -3), this);
-    this.clearColor = new BABYLON.Color3(0.05, 0.05, 0.05);
+    this.light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(3, 1, -3), this);
+    this.clearColor = backgroundConfig.sceneColor;
 
     this.initBackgroundParticles();
     // this.initFog();
@@ -32,21 +31,33 @@ class Scene extends BABYLON.Scene {
 
   initFog () {
     this.fogMode = BABYLON.Scene.FOGMODE_EXP;
-    this.fogDensity = 0.001;
-    this.fogColor = new BABYLON.Color3(0, 0, 0);
+    this.fogDensity = 0.007;
+    this.fogColor = backgroundConfig.sceneColor;
   }
 
   initBackgroundParticles () {
-    const { targetOffset, minEmitBox, maxEmitBox, direction1, direction2, minTime, maxTime } = backgroundConfig;
-    this.backgroundParticles = new Particles('background', targetOffset, this, flare);
-    this.backgroundParticles.setEmitBox(minEmitBox, maxEmitBox);
-    this.backgroundParticles.setDirections(direction1, direction2);
-    this.backgroundParticles.setLifeTimes(minTime, maxTime);
+    const { sceneColor, targetOffset, minEmitBox, maxEmitBox, direction1, direction2, minTime, maxTime } = backgroundConfig;
+
+    const emitterMaterial = new BABYLON.StandardMaterial('emitterMaterial', this);
+    emitterMaterial.emissiveColor = sceneColor;
+
+    const emitter = new BABYLON.Mesh.CreateBox('emitter', 150, this);
+    emitter.position = targetOffset;
+    emitter.material = emitterMaterial;
+
+    const particles = new Particles('background', emitter, this, flare);
+    // particles.setEmitBox(minEmitBox, maxEmitBox);
+    particles.setDirections(direction1, direction2);
+    particles.setLifeTimes(minTime, maxTime);
+
+    this.emitter = emitter;
+    this.light.excludedMeshes = [this.emitter];
+    this.backgroundParticles = particles;
   }
 
   updateBackgroundParticlesEmitter (newPosition) {
-    let { targetOffset } = backgroundConfig;
-    this.backgroundParticles.emitter = newPosition.add(targetOffset);
+    const { targetOffset } = backgroundConfig;
+    this.backgroundParticles.emitter.position = newPosition.add(targetOffset);
   }
 }
 
