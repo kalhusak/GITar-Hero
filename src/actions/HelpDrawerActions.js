d136bf3fc2e5da3741ffa2ccac4892625a713248
Tymoteusz Bleja <tymoteusz.bleja@outlook.com>
2016-12-28 19:27:00 +0100

    Add keyboard navigation for help drawer

--- src/actions/HelpDrawerActions.js
+++ src/actions/HelpDrawerActions.js
@@ -4,8 +4,12 @@ export const OPEN_HELP_DRAWER = 'OPEN_HELP_DRAWER';
 export const CLOSE_HELP_DRAWER = 'CLOSE_HELP_DRAWER';
 export const TOGGLE_HELP_DRAWER = 'TOGGLE_HELP_DRAWER';
 export const SELECT_HELP_DRAWER_TAB = 'SELECT_HELP_DRAWER_TAB';
+export const NEXT_HELP_DRAWER_TAB = 'NEXT_HELP_DRAWER_TAB';
+export const PREV_HELP_DRAWER_TAB = 'PREV_HELP_DRAWER_TAB';
 
 export const openHelpDrawer = () => createAction(OPEN_HELP_DRAWER);
 export const closeHelpDrawer = () => createAction(CLOSE_HELP_DRAWER);
 export const toggleHelpDrawer = () => createAction(TOGGLE_HELP_DRAWER);
+export const nextHelpDrawerTab = () => createAction(NEXT_HELP_DRAWER_TAB);
+export const prevHelpDrawerTab = () => createAction(PREV_HELP_DRAWER_TAB);
 export const selectHelpDrawerTab = (tab) => createAction(SELECT_HELP_DRAWER_TAB, { tab });
--- src/components/BottomDrawer/BottomDrawer.js
+++ src/components/BottomDrawer/BottomDrawer.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { find } from 'lodash';
 import Console from '../Console';
-import { closeHelpDrawer, selectHelpDrawerTab } from '../../actions/HelpDrawerActions';
+import { closeHelpDrawer,
+  selectHelpDrawerTab,
+  nextHelpDrawerTab,
+  prevHelpDrawerTab } from '../../actions/HelpDrawerActions';
 import helpTabs from './helpTabs';
 import './BottomDrawer.scss';
 
@@ -10,6 +13,8 @@ class BottomDrawer extends Component {
   constructor (props) {
     super(props);
     this.closeDrawer = ::this.closeDrawer;
+    this.handleKeyDown = ::this.handleKeyDown;
+    this.selectTab = ::this.selectTab;
     this.initialRender = true;
   }
 
@@ -33,6 +38,40 @@ class BottomDrawer extends Component {
 
   renderCurrentTab () {
     return find(helpTabs, { name: this.props.selectedTab }).content;
+  }
+
+  handleKeyDown ({ target, keyCode }) {
+    const { isOpen, dispatch } = this.props;
+
+    if (isOpen) {
+      const [CODE_ESC, CODE_LEFT, CODE_RIGHT] = [27, 37, 39];
+
+      switch (event.keyCode) {
+        case CODE_ESC:
+          this.closeDrawer();
+          break;
+        case CODE_LEFT:
+          if (target.tagName === 'INPUT' && target.selectionStart > 0) {
+            return;
+          }
+          dispatch(prevHelpDrawerTab());
+          break;
+        case CODE_RIGHT:
+          if (target.tagName === 'INPUT' && target.selectionStart < target.value.length) {
+            return;
+          }
+          dispatch(nextHelpDrawerTab());
+          break;
+      }
+    }
+  }
+
+  componentDidMount () {
+    document.body.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount () {
+    document.body.removeEventListener('keydown', this.handleKeyDown);
   }
 
   render () {
