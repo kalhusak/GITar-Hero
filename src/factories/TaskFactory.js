2729652ac7e173c11db8e9d5de1ac0c80cfee107
imosek8 <imosek8@o2.pl>
2016-12-30 21:06:37 +0100

    Fix points counting

--- src/factories/TaskFactory.js
+++ src/factories/TaskFactory.js
@@ -15,7 +15,7 @@ class TaskFactory {
       newTask.currentStepIndex = 0;
       newTask.time = statisticsUtils.calculateTaskTime(newTask.minTime,
       newTask.defaultTime, taskProvider.getTasksCount());
-      newTask.reward = newTask.minTime * taskProvider.getTasksCount();
+      newTask.reward = newTask.minTime * taskProvider.getTasksCount() * 10;
       newTask.id = taskSequence.nextTask();
     }
     return newTask;
