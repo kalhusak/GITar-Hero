6a09f1a78e631d910f4f35147e23012af41bae6c
Tymoteusz Bleja <tymoteusz.bleja@outlook.com>
2016-12-30 02:01:51 +0100

    Adjust time component to fit in task block

--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import './Time.scss';
 
 const transitionTime = 3000;
 
 export default class Time extends Component {
+  static propTypes = {
+    active: PropTypes.bool,
+    label: PropTypes.string,
+    time: PropTypes.number.isRequired
+  };
+
   constructor (props) {
     super(props);
     this.state = {
-      timeValue: 0,
-      rewardValue: 0,
-      rewardInitialValue: 0
+      timeValue: 1
     };
     this.animateBar = ::this.animateBar;
   }
@@ -19,7 +23,7 @@ export default class Time extends Component {
   }
 
   getColor (t) {
-    return `hsl(${t * 140}, 70%, 50%)`;
+    return `hsl(${t * 120}, 50%, 50%)`;
   }
 
   animateBar () {
@@ -32,49 +36,48 @@ export default class Time extends Component {
     }
 
     this.setState({
-      rewardValue: 1 - this.ease((now - start) / transitionTime),
       timeValue:  1 - Math.min((now - start) / time, 1)
     });
   }
 
-  componentWillReceiveProps () {
-    this.setState({
-      rewardInitialValue: this.state.timeValue,
-      timeValue: 1,
-      start: Date.now()
-    });
+  startIfActive (active) {
+    if (active) {
+      this.setState({
+        timeValue: 1,
+        start: Date.now()
+      });
 
-    requestAnimationFrame(this.animateBar);
+      requestAnimationFrame(this.animateBar);
+    }
   }
 
-  renderReward () {
-    const { rewardValue, rewardInitialValue } = this.state;
-    if (rewardValue > 0) {
-      const rewardState = rewardValue * rewardInitialValue;
-      const styles = {
-        transform: `scaleX(${rewardState})`,
-        backgroundColor: this.getColor(rewardInitialValue),
-        opacity: 0.75 + rewardValue / 4
-      };
+  componentWillReceiveProps ({ active }) {
+    this.startIfActive(active);
+  }
 
-      return <div className='time-container__bar' style={styles} />;
-    }
+  componentDidMount () {
+    this.startIfActive(this.props.active);
   }
 
   renderTime () {
     const { timeValue } = this.state;
-    const styles = {
-      transform: `scaleX(${timeValue})`,
+    const { label } = this.props;
+    const barStyle = {
+      clipPath: `inset(0px ${(1 - timeValue) * 100}% 0px 0px)`,
       backgroundColor: this.getColor(timeValue)
     };
 
-    return <div className='time-container__bar' style={styles} />;
+    return <div className='time__bar' style={barStyle}>
+      <div className='time__label time__label--white'>{label}</div>
+    </div>;
   }
 
   render () {
-    return <div className='time-container'>
+    const { label } = this.props;
+
+    return <div className='time'>
+      <div className='time__label'>{label}</div>
       { this.renderTime() }
-      { this.renderReward() }
     </div>;
   }
 }
