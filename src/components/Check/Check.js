5ea79e9ff4d8861fc7fda37d6556291de7a262cd
Tymoteusz Bleja <tymoteusz.bleja@outlook.com>
2017-01-02 11:28:03 +0100

    Add Check component size property

--- src/components/Check/Check.js
+++ src/components/Check/Check.js
@@ -3,16 +3,22 @@ import './Check.scss';
 
 export default class Check extends Component {
 
+  static defaultSize = 30;
   static propTypes = {
-    checked: PropTypes.bool.isRequired
+    checked: PropTypes.bool.isRequired,
+    size: PropTypes.number
   };
 
   render () {
-    const { checked } = this.props;
+    const { checked, size } = this.props;
     const pathDef = 'M14 32 L30 54 L52 10 Z';
+    const checkStyle = {
+      width: `${size || Check.defaultSize}px`,
+      height: `${size || Check.defaultSize}px`
+    };
 
     return (
-      <svg className='check' viewBox='0 0 64 64'>
+      <svg viewBox='0 0 64 64' style={checkStyle}>
         <path className='check__path check__path--bg' d={pathDef} />
         <path className={'check__path' + ((checked) ? ' check__path--checked' : '')} d={pathDef} />
       </svg>
