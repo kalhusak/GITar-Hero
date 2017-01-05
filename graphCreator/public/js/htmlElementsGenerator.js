4c50e192a3220334a736a5ca85e0ce5c1a6f4b69
Pawel Husak <pawelhusak@gmail.com>
2017-01-05 18:31:25 +0100

    Add new/removed files to actions

--- graphCreator/public/js/htmlElementsGenerator.js
+++ graphCreator/public/js/htmlElementsGenerator.js
@@ -9,12 +9,13 @@ function createCheckoutStepElement (step, index, label) {
   var additionalProperties = [];
   additionalProperties.push(createPropertyElement('toCommitOrBranchInput' + index, 'Commit/Branch', step, 'toCommitOrBranch'));
   additionalProperties.push(createPropertyElement('nameInput' + index, 'Name', step, 'name'));
+  additionalProperties.push(createPropertyElement('newFilesInput' + index, 'New files', step, 'newFiles'));
+  additionalProperties.push(createPropertyElement('removeFilesInput' + index, 'Remove files', step, 'removeFiles'));
   return createStepElement(step, index, label, additionalProperties);
 }
 
 function createAddStepElement (step, index, label) {
   var additionalProperties = [];
-  additionalProperties.push(createPropertyElement('newFilesInput' + index, 'New files', step, 'newFiles'));
   additionalProperties.push(createPropertyElement('modifyFilesInput' + index, 'Modify files', step, 'modifyFiles'));
   additionalProperties.push(createPropertyElement('removeFilesInput' + index, 'Remove files', step, 'removeFiles'));
   return createStepElement(step, index, label, additionalProperties);
@@ -30,6 +31,8 @@ function createMergeStepElement (step, index, label) {
   var additionalProperties = [];
   additionalProperties.push(createPropertyElement('sourceBranchInput' + index, 'Source', step, 'sourceBranch'));
   additionalProperties.push(createPropertyElement('targetBranchInput' + index, 'Target', step, 'targetBranch'));
+  additionalProperties.push(createPropertyElement('newFilesInput' + index, 'New files', step, 'newFiles'));
+  additionalProperties.push(createPropertyElement('removeFilesInput' + index, 'Remove files', step, 'removeFiles'));
   return createStepElement(step, index, label, additionalProperties);
 }
 
@@ -37,6 +40,8 @@ function createResetStepElement (step, index, label) {
   var additionalProperties = [];
   additionalProperties.push(createPropertyElement('commitNumberInput' + index, 'Commit/Number', step, 'commitOrNumber'));
   additionalProperties.push(createPropertyElement('valueInput' + index, 'Value', step, 'value'));
+  additionalProperties.push(createPropertyElement('newFilesInput' + index, 'New files', step, 'newFiles'));
+  additionalProperties.push(createPropertyElement('removeFilesInput' + index, 'Remove files', step, 'removeFiles'));
   return createStepElement(step, index, label, additionalProperties);
 }
 
@@ -44,10 +49,16 @@ function createPullStepElement (step, index, label) {
   var additionalProperties = [];
   additionalProperties.push(createPropertyElement('commitName1Input' + index, '1. name', step, 'commitName1'));
   additionalProperties.push(createPropertyElement('commitMessage1Input' + index, '1. message', step, 'commitMessage1'));
+  additionalProperties.push(createPropertyElement('newFiles1Input' + index, '1.  new files', step, 'newFiles1'));
+  additionalProperties.push(createPropertyElement('removeFiles1Input' + index, '1. remove files', step, 'removeFiles1'));
   additionalProperties.push(createPropertyElement('commitName2Input' + index, '2. name', step, 'commitName2'));
   additionalProperties.push(createPropertyElement('commitMessage2Input' + index, '2. message', step, 'commitMessage2'));
+  additionalProperties.push(createPropertyElement('newFiles2Input' + index, '2. new files', step, 'newFiles2'));
+  additionalProperties.push(createPropertyElement('removeFiles2Input' + index, '2. remove files', step, 'removeFiles2'));
   additionalProperties.push(createPropertyElement('commitName3Input' + index, '3. name', step, 'commitName3'));
   additionalProperties.push(createPropertyElement('commitMessage3Input' + index, '3. message', step, 'commitMessage3'));
+  additionalProperties.push(createPropertyElement('newFiles3Input' + index, '3. new files', step, 'newFiles3'));
+  additionalProperties.push(createPropertyElement('removeFiles3Input' + index, '3. remove files', step, 'removeFiles3'));
   return createStepElement(step, index, label, additionalProperties);
 }
 
