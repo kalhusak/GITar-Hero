13085475ca45e2f23047fd182e3db9e7300dc760
Pawel Husak <pawelhusak@gmail.com>
2017-01-12 12:58:17 +0100

    Add reset to allowed commands

--- src/config.js
+++ src/config.js
@@ -2,14 +2,15 @@ const config = {
   noCommandValidation: false, // option to debug whole tasks path just by pressing enter
   taskListSize: 2,
   allowedCommands: [
-    'init',
-    'add :file:',
-    'merge :branch:',
-    'push',
-    'commit',
-    'branch',
-    'checkout :branch:',
-    'rebase :branch: :branch:']
+    'init ',
+    'add :file: ',
+    'merge :branch: ',
+    'push ',
+    'commit ',
+    'branch ',
+    'checkout :branch: ',
+    'rebase :branch: :branch: ',
+    'reset ']
 };
 
 export default config;
