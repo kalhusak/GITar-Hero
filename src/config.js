c5793dce73d7aa00184dfd4cd3cda6223e92533e
MLatkowska <magda@laszlo.pl>
2017-01-21 23:42:01 +0100

    Change next task generation to choose new task once previous one is done

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   noCommandValidation: false, // option to debug whole tasks path just by pressing enter
-  taskListSize: 2,
+  taskListSize: 1,
   allowedCommands: [
     'init ',
     'add :file: ',
