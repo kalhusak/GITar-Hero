f25983fd4e2b5eb52ffb808cf040a5da8e8f2ab5
MLatkowska <magda@laszlo.pl>
2017-01-22 01:40:52 +0100

    Change task weight calculation

--- src/utils/StatisticsUtils.js
+++ src/utils/StatisticsUtils.js
@@ -1,17 +1,38 @@
 import _ from 'lodash';
 
 function calculateWeight (tagsMap, taskTags) {
-  return _.reduce(taskTags, (sum, taskTag) => {
-    return tagsMap[taskTag] ? sum + countTaskTagWeight(tagsMap[taskTag].ratio) : sum;
-  }, 1) * 100;
+  let weight = 0;
+  let knownTags = 0;
+
+  for (let taskTag of taskTags) {
+    if (tagsMap[taskTag] && (tagsMap[taskTag].ratio === 1)) {
+      knownTags++;
+    }
+    weight += countTaskTagWeight(tagsMap, taskTag);
+  }
+
+  weight = weight * 100 / Math.max(1, (taskTags.length - knownTags));
+  return Math.max(weight, 1);
 }
 
 function calculateTagRatio (probes, valid) {
   return (valid || 0) / probes;
 }
 
-function countTaskTagWeight (ratio) {
-  return Math.pow(1 / (ratio || 1), 2);
+/**
+ * Returns tag weight:
+ *  - new tag           : 1
+ *  - tag ratio  0-25%  : 16
+ *  - tag ratio 26-99%  : (1,16)
+ *  - tag ratio   100%  : 0
+ */
+function countTaskTagWeight (tagsMap, tag) {
+  if (tagsMap[tag]) {
+    const ratio = tagsMap[tag].ratio;
+    return ratio >= 1 ? 0 : Math.pow(1 / Math.max(ratio, 0.25), 2);
+  } else {
+    return 1;
+  }
 }
 
 function calculateTaskReward (task, elapsedTime) {
