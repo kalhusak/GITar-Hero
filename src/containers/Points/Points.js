a7fc5f37fd64d030ba871e9e87a861af24025d4a
Tymoteusz Bleja <tymoteusz.bleja@outlook.com>
2017-01-25 22:31:13 +0100

    Connect SpeedMeter with state

--- src/containers/Points/Points.js
+++ src/containers/Points/Points.js
@@ -6,9 +6,18 @@ import ProgressLines from '../../components/ProgressLines';
 import './Points.scss';
 
 class Points extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      time: 1,
+      from: Date.now()
+    };
+    this.setSpeedMeterValue = ::this.setSpeedMeterValue;
+  }
+
   renderSpeedMeter () {
     return <div className='top__speed-meter'>
-      <SpeedMeter value={0.4} />
+      <SpeedMeter value={this.state.time} />
     </div>;
   }
 
@@ -27,6 +36,33 @@ class Points extends Component {
     </div>;
   }
 
+  setSpeedMeterValue () {
+    const time = Math.max(1 - (Date.now() - this.state.from) / (this.props.time * 1000), 0);
+
+    if (time !== this.state.time) {
+      requestAnimationFrame(() => {
+        this.setState({ time });
+      });
+    }
+  }
+
+  componentDidMount () {
+    this.setTimeInterval = setInterval(this.setSpeedMeterValue, 100);
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.setTimeInterval);
+  }
+
+  componentWillReceiveProps ({ time }) {
+    if (time !== this.props.time) {
+      this.setState({
+        from: Date.now(),
+        time: 1
+      });
+    }
+  }
+
   render () {
     return <div className='points-container'>
       {this.renderLines()}
@@ -39,10 +75,11 @@ class Points extends Component {
 export default connect(({ points, tasks }) => {
   const task = tasks.byId[tasks.current];
   const progress = task ? task.currentStepIndex / task.steps.length : 0;
+  const time = task ? task.time : 0;
 
   return {
     points: points.value,
     progress,
-    time: task.time
+    time
   };
 })(Points);
