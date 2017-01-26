39afcae462c79f2b1f122f8654e5280f7fac7362
Tymoteusz Bleja <tymoteusz.bleja@outlook.com>
2017-01-26 18:42:17 +0100

    Add and style bottom drawer info

--- src/actions/HelpDrawerActions.js
+++ src/actions/HelpDrawerActions.js
@@ -7,6 +7,7 @@ export const SELECT_HELP_DRAWER_TAB = 'SELECT_HELP_DRAWER_TAB';
 export const NEXT_HELP_DRAWER_TAB = 'NEXT_HELP_DRAWER_TAB';
 export const PREV_HELP_DRAWER_TAB = 'PREV_HELP_DRAWER_TAB';
 export const TOGGLE_AUTO_SHOW_OPTION = 'TOGGLE_AUTO_SHOW_OPTION';
+export const SHOW_HELP_DRAWER_INFO = 'SHOW_HELP_DRAWER_INFO';
 
 export const openHelpDrawer = () => createAction(OPEN_HELP_DRAWER);
 export const closeHelpDrawer = () => createAction(CLOSE_HELP_DRAWER);
@@ -15,4 +16,4 @@ export const nextHelpDrawerTab = () => createAction(NEXT_HELP_DRAWER_TAB);
 export const prevHelpDrawerTab = () => createAction(PREV_HELP_DRAWER_TAB);
 export const toggleAutoShowHelp = () => createAction(TOGGLE_AUTO_SHOW_OPTION);
 export const selectHelpDrawerTab = (tab) => createAction(SELECT_HELP_DRAWER_TAB, { tab });
-export const autoOpenHelpDrawerTab = (tab) => createAction(SELECT_HELP_DRAWER_TAB, { tab, auto: true });
+export const autoOpenHelpDrawerTab = (tab) => createAction(SHOW_HELP_DRAWER_INFO, { tab });
