f270c4e0a52dcc51e19c67a9f3d38f3a6ac02d18
Tymoteusz Bleja <tymoteusz.bleja@outlook.com>
2017-01-27 15:56:54 +0100

    Add tutorial message component

--- src/components/BlurItem/BlurItem.js
+++ src/components/BlurItem/BlurItem.js
@@ -1,12 +1,24 @@
 import React, { PropTypes } from 'react';
+import './BlurItem.scss';
 
 export default function BlurItem (props) {
-  return <div className={props.noBlur ? 'tutorial__active-item' : ''} style={props.style}>
+  const classes = ['blur-item'];
+
+  if (props.blur) {
+    classes.push('blur-item--blur');
+  }
+
+  if (!props.active) {
+    classes.push('blur-item--inactive');
+  }
+
+  return <section className={classes.join(' ')} style={props.style}>
     {props.children}
-  </div>;
+  </section>;
 }
 
 BlurItem.propTypes = {
-  noBlur: PropTypes.bool.isRequired,
+  blur: PropTypes.bool.isRequired,
+  active: PropTypes.bool,
   style: PropTypes.object
 };
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -5,9 +5,8 @@ import BottomDrawer from './BottomDrawer';
 import CurrentTask from './CurrentTask';
 import Top from './Top';
 import Tree from './Tree';
+import Tutorial from './Tutorial';
 import BlurItem from '../components/BlurItem';
-import Blur from '../components/Blur';
-import { closeTutorial } from '../actions/TutorialActions';
 import './App.scss';
 
 class App extends Component {
@@ -15,49 +14,41 @@ class App extends Component {
     store: PropTypes.object.isRequired
   }
 
-  constructor (props) {
-    super(props);
-    this.onTutorialClose = ::this.onTutorialClose;
+  blurComponent (tutorialName) {
+    return this.props.tutorial && this.props.tutorial !== tutorialName ||
+      this.props.helpDrawer.isOpen && tutorialName !== 'console';
   }
 
-  onTutorialClose () {
-    this.props.dispatch(closeTutorial());
+  disableComponent () {
+    return Boolean(this.props.tutorial);
   }
+
+  // <BlurItem blur={this.blurComponent()} active={!this.disableComponent()}>
+  //   <Canvas store={this.props.store} />
+  // </BlurItem>
 
   render () {
     return (
       <Provider store={this.props.store}>
         <div className='app'>
-          <Blur blur={this.props.helpDrawer.isOpen}
-            message={null} onClose={this.onTutorialClose}>
-            <Canvas store={this.props.store} />
+
+          <BlurItem blur={this.blurComponent()} active={!this.disableComponent()}>
             <Top />
+          </BlurItem>
+          <BlurItem blur={this.blurComponent('tree')} active={!this.disableComponent()}>
             <Tree />
-            <BlurItem enabled={this.props.tutorial === 'console' || this.props.helpDrawer.isOpen} style={{ height: '100%' }}>
-              <BottomDrawer />
-            </BlurItem>
+          </BlurItem>
+          <BlurItem blur={this.blurComponent('taskList')} active={!this.disableComponent()}>
             <CurrentTask />
-          </Blur>
+          </BlurItem>
+          <BlurItem blur={this.blurComponent('console')} active={!this.disableComponent()}>
+            <BottomDrawer />
+          </BlurItem>
+          <Tutorial />
         </div>
       </Provider>
     );
   }
-
-  // <Tutorial blur={this.props.helpDrawer.isOpen}
-  //   message={null} onClose={this.onTutorialClose}>
-
-  // <Tutorial show={this.props.tutorial} onClose={this.onTutorialClose}>
-  // <Canvas store={this.props.store} />
-  // <TutorialItem enabled={this.props.tutorial === 'taskList'}>
-  //   <TaskList />
-  // </TutorialItem>
-  // <TutorialItem enabled={this.props.tutorial === 'console'}>
-  //   <BottomDrawer />
-  // </TutorialItem>
-  // <TutorialItem enabled={this.props.tutorial === 'tree'}>
-  //   <Tree />
-  // </TutorialItem>
-  // </Tutorial>
 }
 
 export default connect(({ tutorial, helpDrawer }) => {
