909897b74f85a239b8c8f4f4b3dfa4a0c5bc5609
Tymoteusz Bleja <tymoteusz.bleja@outlook.com>
2017-01-27 19:11:45 +0100

    Unify container names

--- src/containers/App.js
+++ src/containers/App.js
@@ -23,15 +23,13 @@ class App extends Component {
     return Boolean(this.props.tutorial);
   }
 
-  // <BlurItem blur={this.blurComponent()} active={!this.disableComponent()}>
-  //   <Canvas store={this.props.store} />
-  // </BlurItem>
-
   render () {
     return (
       <Provider store={this.props.store}>
         <div className='app'>
-
+          <BlurItem blur={this.blurComponent()} active={!this.disableComponent()}>
+            <Canvas store={this.props.store} />
+          </BlurItem>
           <BlurItem blur={this.blurComponent()} active={!this.disableComponent()}>
             <Top />
           </BlurItem>
